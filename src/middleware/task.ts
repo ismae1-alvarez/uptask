3d9ae99c501d2d993e70c2c5d75a3500ec92584e
Ismael Alvarez <ismaelalvarez514@gmail.com>
2024-08-16 23:23:59 -0600

    Repairs controller

--- src/middleware/task.ts
+++ src/middleware/task.ts
@@ -1,5 +1,6 @@
 import type {Request, Response, NextFunction} from 'express';
 import Task ,{ ITask } from '../models/Tasks';
+import mongoose from 'mongoose';
 
 declare global {
     namespace Express{
@@ -13,20 +14,23 @@ declare global {
 
 export async function taksExists(req:Request, res:Response, next:NextFunction) {
     try {
-        const {taskId} =  req.params;
+        const { taskId } = req.params;
 
-        const task =  await Task.findById(taskId);
-        
-        if(!task){
-            const error =  new Error('Tarea no encontrada no encontrado');
-            return res.status(404).json({error: error.message});
-        };
+        if (!mongoose.Types.ObjectId.isValid(taskId)) {
+            return res.status(400).json({ error: 'ID de tarea no válido' });
+        }
+
+        const task = await Task.findById(taskId);
+
+        if (!task) {
+            return res.status(404).json({ error: 'Tarea no encontrada' });
+        }
 
         req.task =  task
 
         next();
     } catch (error) {
-        res.status(500).json({error : 'Hubo un error'});
+        res.status(500).json({error : 'Hubo un error 2'});
     };
 };
 
