a47efeda82756870c75adabf3d96207fd3c694c5
Ismael Alvarez <ismaelalvarez514@gmail.com>
2024-08-17 22:26:57 -0600

    Integrate url in email

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -1,5 +1,6 @@
 import { transporte } from "../config/nodemailer";
 
+
 interface IEmail{
     email: string;
     name : string;
@@ -18,7 +19,7 @@ export class AuthEmail {
             text : 'UpTask - Confirma tu cuenta',
             html: `<p>Hola: ${user.email}, has creado tu cuenta en UpTask, yas casi esta listo, solo debes de onfirmar tu cuenta</p>
             <p>Visita el siguiente enlace:</p>
-            <a href=""> Confirma cuenta</a>
+            <a href="${process.env.FRONTEND_URL}/auth/confirm-account"> Confirma cuenta</a>
             <p>E ingresa el codigo: <b>${user.token}</b></p>
             <p>Este token expira en 10 minutos</p>
             `
